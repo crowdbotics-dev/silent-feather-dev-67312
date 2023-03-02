b4f96792ec70d4a60b0f1c7c0de5ebde0b1f6771
Crowdbotics <engineering@crowdbotics.com>
2023-03-02 00:08:32 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "silent-feather-dev-67312",
+  url: "https://silent-feather-dev-67312.botics.co",
+  api: "https://silent-feather-dev-67312.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
